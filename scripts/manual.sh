d35587b6ae90f84ee317bc9e5fa82de5bbce7a20
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-24 11:35:19 +0000

    Feat: update manual file
    - Update manual file to correctly join the pods to the vault cluster.
    - Increase log transparency of the script.

--- scripts/manual.sh
+++ scripts/manual.sh
@@ -1,20 +1,46 @@
-rm -rf artifacts
-mkdir -p artifacts
-for i in 0 1 2; do
+artifacts_folder=artifacts
+rm -rf $artifacts_folder
+mkdir -p $artifacts_folder
+for i in {0..2}; do
+  sleep 3;
   echo "Starting unseal for 'vault-$i'…"
-  artifact_file="artifacts/init.$i.json"
-  kubectl -n vault exec "vault-$i" -- vault operator init \
-    -key-shares=5 \
-    -key-threshold=3 \
-    -format=json > \
-    $artifact_file 
-  if [ -z "$(cat artifacts/init.$i.json)" ]; then
-    echo "Artifact file '$artifact_file' is empty"
+  artifact_file="$artifacts_folder/init.json"
+  if [ "$i" == 0 ]; then
+    if [ ! -f $artifact_file ] || [ -z "$(cat $artifact_file)" ]; then
+      kubectl -n vault exec "vault-$i" -- vault operator init \
+        -key-shares=5 \
+        -key-threshold=3 \
+        -format=json > \
+        $artifact_file 
+    fi
+  fi
+  if [ -z "$(cat $artifact_file)" ]; then
+    echo "Error: Artifact file '$artifact_file' is empty"
     continue
+  fi
+  if [ "$i" != "0" ]; then
+  echo "Joining $i to '0'…"
+  kubectl -n vault exec "vault-$i" -- vault operator raft join http://vault-0.vault-internal:8200
   fi
   unseal_keys=$(jq -r '.unseal_keys_b64[0:3][]' $artifact_file)
   for key in $unseal_keys; do
-    kubectl -n vault exec "vault-$i" -- vault operator unseal $key
+    echo "vault-$i key $key"
+    sleep 3;
+    kubectl -n vault exec "vault-$i" -- vault operator unseal "$key"
   done
+  if [ "$i" == 0 ]; then
+    echo "Logging in '$i'…"
+    root_token=$(jq -r '.root_token' $artifact_file)
+    if [ -z "$root_token" ]; then 
+      echo "Error: Root token is missing for $i"
+      continue
+    fi
+    kubectl -n vault exec "vault-$i" -- vault login "$root_token"
+  fi
   echo "Finished unseal for 'vault-$i'"
+  echo
+done
+sleep 3;
+for i in {0..2}; do
+kubectl -n vault exec "vault-$i" -- vault status;
 done
